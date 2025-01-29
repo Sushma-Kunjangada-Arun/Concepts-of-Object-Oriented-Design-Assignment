bf6800c37f925f803f68c02887b4f4c8faac0fef
Sushma Kunjangada Arun <sushma@LOANERHXJMJ587265F.tld>
2025-01-29 11:42:00 -0500

    Lab 2

---
+++ Lab2/src/edu/neu/csye6200/GunShotA.java
@@ -0,0 +1,11 @@
+package edu.neu.csye6200;
+
+public class GunShotA extends AbstractExplosion{
+
+	@Override
+	public void explode() {
+		// TODO Auto-generated method stub
+		System.out.println("GunShotA: Bang!");
+	}
+
+}
