51ca6c100a67446332e22048b09a06c63bef3aa9
Sushma Kunjangada Arun <sushma@LOANERHXJMJ587265F.tld>
2025-04-10 15:50:00 -0400

    Lab

---
+++ Lab/Lab2/src/edu/neu/csye6200/ExplosionModel.java
@@ -0,0 +1,8 @@
+package edu.neu.csye6200;
+
+public class ExplosionModel {
+	public void triggerExplosion(Explosion explosion) {
+        explosion.explode();
+    }
+
+}
---
+++ Lab/Lab2/src/edu/neu/csye6200/Grenade.java
@@ -0,0 +1,8 @@
+package edu.neu.csye6200;
+
+public class Grenade extends Explosion {
+	@Override
+    public void explode() {
+        System.out.println("Grenade: Kaboom!");
+    }
+}
