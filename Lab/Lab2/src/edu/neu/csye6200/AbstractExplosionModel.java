246da9cc4bbe41f79a0382fb07a343dcbbe0247e
Sushma-Kunjangada-Arun <156254258+Sushma-Kunjangada-Arun@users.noreply.github.com>
2025-05-05 13:31:55 -0400

    Delete Lab directory

--- Lab/Lab2/src/edu/neu/csye6200/AbstractExplosionModel.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.csye6200;
-
-public class AbstractExplosionModel {
-	public void triggerExplosion(AbstractExplosion explosion) {
-        explosion.explode();
-    }
-}
--- Lab/Lab2/src/edu/neu/csye6200/ExplodableModel.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.csye6200;
-
-public class ExplodableModel {
-	public void triggerExplosion(Explodable explodable) {
-        explodable.explode();
-    }
-}
